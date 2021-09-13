b3d80406c88955f1133da307c0c73535b505692f
romedal <romedal7@gmail.com>
2021-09-14 00:43:57 +0300

    Test window

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
-// arrays.rs
+extern crate sdl2;
 
-fn main() { 
-    let numbers: [u8; 10] = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11]; 
-    let floats = [0.1f64, 0.2, 0.3]; 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+use std::thread::sleep;
 
-    println!("Number: {}", numbers[5]);
-    println!("Float: {}", floats[2]);
+pub fn main() {
+  let sdl_context = sdl2::init().expect("SDL initialization   
+  failed");
+  let video_subsystem = sdl_context.video().expect("Couldn't get 
+   SDL video subsystem");
+
+  let window = video_subsystem.window("rust-sdl2 demo: Video", 800,
+        600)
+    .position_centered()
+    .opengl()
+    .build()
+    .expect("Failed to create window");
+
+  let mut canvas = window.into_canvas().build().expect("Failed to
+    convert window into canvas");
+
+  canvas.set_draw_color(Color::RGB(255, 0, 0));
+  canvas.clear();
+  canvas.present();
+  let mut event_pump = sdl_context.event_pump().expect("Failed to
+    get SDL event pump");
+
+  'running: loop {
+     for event in event_pump.poll_iter() {
+        match event {
+          Event::Quit { .. } |
+          Event::KeyDown { keycode: Some(Keycode::Escape), .. } =>  
+          {
+            break 'running
+          },
+          _ => {}
+        }
+     }
+     sleep(Duration::new(0, 1_000_000_000u32 / 60));
+  }
 }
