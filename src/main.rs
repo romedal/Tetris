8e100ddde7fbf27cdde4b137dbaf5afc3f71bae3
romedal <romedal7@gmail.com>
2021-09-15 01:22:43 +0300

    Working with image

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,47 @@
 extern crate sdl2;
-
 use sdl2::event::Event;
+use sdl2::image::{LoadTexture, INIT_JPG, INIT_PNG};
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::Rect;
-use sdl2::render::{Canvas, Texture, TextureCreator};
-use sdl2::video::{Window, WindowContext};
+use sdl2::render::TextureCreator;
+use std::time::Duration;
 
-use std::thread::sleep;
-use std::time::{Duration, SystemTime};
-
-const TEXTURE_SIZE: u32 = 32;
-
-#[derive(Clone, Copy)]
-enum TextureColor {
-  Green,
-  Blue,
-}
-
-fn create_texture_rect<'a>(
-  canvas: &mut Canvas<Window>,
-  texture_creator: &'a TextureCreator<WindowContext>,
-  color: TextureColor,
-  size: u32,
-) -> Option<Texture<'a>> {
-  if let Ok(mut square_texture) = texture_creator.create_texture_target(None, size, size) {
-    canvas
-      .with_texture_canvas(&mut square_texture, |texture| {
-        match color {
-          TextureColor::Green => texture.set_draw_color(Color::RGB(0, 255, 0)),
-          TextureColor::Blue => texture.set_draw_color(Color::RGB(0, 0, 255)),
-        }
-        texture.clear();
-      })
-      .expect("Failed to color a texture");
-    Some(square_texture)
-  } else {
-    None
-  }
-}
-
-fn main() {
+pub fn main() {
   let sdl_context = sdl2::init().expect(
-    "SDL initialization  
+    "SDL initialization 
    failed",
   );
   let video_subsystem = sdl_context.video().expect(
-    "Couldn't get 
-      SDL video subsystem",
+    "Couldn't 
+     get SDL video subsystem",
+  );
+
+  sdl2::image::init(INIT_PNG | INIT_JPG).expect(
+    "Couldn't  
+  initialize
+    image context",
   );
 
   let window = video_subsystem
-    .window("Tetris", 800, 600)
+    .window("rust-sdl2 image demo", 800, 600)
     .position_centered()
+    .opengl()
     .build()
     .expect("Failed to create window");
 
-  let mut canvas = window
-    .into_canvas()
-    .target_texture()
-    .present_vsync()
-    .build()
-    .expect("Couldn't get window's canvas");
+  let mut canvas = window.into_canvas().build().expect(
+    "Failed to 
+    convert window into canvas",
+  );
 
   let texture_creator: TextureCreator<_> = canvas.texture_creator();
-
-  let green_square = create_texture_rect(
-    &mut canvas,
-    &texture_creator,
-    TextureColor::Green,
-    TEXTURE_SIZE,
-  )
-  .expect("Failed to create a texture");
-  let blue_square = create_texture_rect(
-    &mut canvas,
-    &texture_creator,
-    TextureColor::Blue,
-    TEXTURE_SIZE,
-  )
-  .expect("Failed to create a texture");
-
-  let timer = SystemTime::now();
+  let image_texture = texture_creator
+    .load_texture("assets/my_image.jpeg")
+    .expect("Couldn't load image");
 
   let mut event_pump = sdl_context.event_pump().expect(
-    "Failed
-     to get SDL event pump",
+    "Failed to
+     get SDL event pump",
   );
 
   'running: loop {
@@ -98,28 +55,13 @@ fn main() {
         _ => {}
       }
     }
-
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
-
-    let display_green = match timer.elapsed() {
-      Ok(elapsed) => elapsed.as_secs() % 2 == 0,
-      Err(_) => true,
-    };
-    let square_texture = if display_green {
-      &green_square
-    } else {
-      &blue_square
-    };
-    canvas
-      .copy(
-        square_texture,
-        None,
-        Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE),
-      )
-      .expect("Couldn't copy texture into window");
+    canvas.copy(&image_texture, None, None).expect(
+      "Render 
+     failed",
+    );
     canvas.present();
-
-    sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
   }
 }
